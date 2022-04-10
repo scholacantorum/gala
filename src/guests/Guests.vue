0b1ef7725143127f6879e0c5691ed2d4eb21c4c4
Steve Roth <steve@rothskeller.net>
2022-04-10 08:55:41 -0700

    Parameterize backend URL.

--- src/guests/Guests.vue
+++ src/guests/Guests.vue
@@ -3,40 +3,22 @@ Guest displays the Guests tab.
 -->
 
 <template lang="pug">
-v-container(fluid fill-height)
+v-container(fluid, fill-height)
   v-layout
-    GuestList(
-      :selected="selected"
-      @select="select"
-    )
+    GuestList(:selected='selected', @select='select')
     keep-alive
       GuestPanel(
-        v-if="selected || adding"
-        :guestID="selected ? selected.id : 0"
-        @done="selected = adding = null"
-        @dirty="dirty = $event"
+        v-if='selected || adding',
+        :guestID='selected ? selected.id : 0',
+        @done='selected = adding = null',
+        @dirty='dirty = $event'
       )
-      div(v-else :class="$style.buttons")
-        v-btn(
-          color="indigo" dark
-          @click="addGuest"
-        ) Add Guest
-        v-btn(
-          color="indigo" dark
-          @click="checkinForms"
-        ) Check-In Forms
-        v-btn(
-          color="indigo" dark
-          @click="programLabels"
-        ) Program Labels
-        v-btn(
-          color="indigo" dark
-          @click="exportPurchases"
-        ) Export Purchases
-        v-btn(
-          color="indigo" dark
-          @click="renderReceipts"
-        ) Print Receipts
+      div(v-else, :class='$style.buttons')
+        v-btn(color='indigo', dark, @click='addGuest') Add Guest
+        v-btn(color='indigo', dark, @click='checkinForms') Check-In Forms
+        v-btn(color='indigo', dark, @click='programLabels') Program Labels
+        v-btn(color='indigo', dark, @click='exportPurchases') Export Purchases
+        v-btn(color='indigo', dark, @click='renderReceipts') Print Receipts
 </template>
 
 <script>
@@ -56,16 +38,23 @@ export default {
       this.adding = true
     },
     checkinForms() {
-      location.href = '/backend/guests/checkin-forms'
+      location.href = `${process.env.VUE_APP_BACKEND_URL
+        }/guests/checkin-forms?auth=${this.$store.state.authToken}`
     },
     programLabels() {
-      location.href = '/backend/guests/program-labels'
+      location.href = `${process.env.VUE_APP_BACKEND_URL
+        }/guests/program-labels?auth=${this.$store.state.authToken}`
     },
     exportPurchases() {
-      location.href = '/backend/purchases/export'
+      location.href = `${process.env.VUE_APP_BACKEND_URL
+        }/purchases/export?auth=${this.$store.state.authToken}`
     },
     renderReceipts() {
-      window.open('/backend/guests/receipts', '_blank')
+      window.open(
+        `${process.env.VUE_APP_BACKEND_URL}/guests/receipts?auth=${this.$store.state.authToken
+        }`,
+        '_blank'
+      )
     },
     select(g) {
       if (!this.dirty) this.selected = g
